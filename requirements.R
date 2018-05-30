aa8f38ba5497cb29b460137a12a2ba5788dc4d94
sharedgit162 <39710355+sharedgit162@users.noreply.github.com>
2018-05-30 12:03:30 +0300

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -3,6 +3,9 @@ library(remotes)
 ##### Install specific package versions from CRAN #####
 
 install_version("ggplot2", "2.2.1")
+install_version("datasets", "3.6.0")
+install_version("corrplot", "0.84")
+install_version("psych", "1.8.4")
 # install_version("dplyr", "0.7.5")
 # install_version("tidyr", "0.8.1")
 # install_version("purrr", "0.2.4")
